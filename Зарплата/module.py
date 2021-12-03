e6b6b2a2ce8950525615be9be266f849d8754913
opilane <opilane@HP-CZC6297B8P>
2021-12-03 10:46:33 +0200

    Сделал комментарии

--- Зарплата/module.py
+++ Зарплата/module.py
@@ -1,36 +1,36 @@
 def dopol(mon,inim):
     '''Добавляет нового человека и его зарплату
     '''
-    while 1:
-        use=input('Sisesta inimene:')
-        if use not in inim:
+    while 1: #бесконечный цикл
+        use=input('Sisesta inimene:') #вводим челоека
+        if use not in inim:     #проверка
             print('Tunnus soobib')
-        zap=input('Sisesta raha:')
-        mon.append(zap)
-        inim.append(use)
-        break
-    return mon,inim
-def koik(inim,mon):
+        zap=int(input('Sisesta raha:'))   #вводим сумму
+        mon.append(zap)  #добавляем зарплату в список
+        inim.append(use) #добавляем человека в список
+        break #Заканчиваем бесконечный цикл
+    return mon,inim #возвращяем списки
+def koik(inim,mon): #создаём функцию
 	'''Показывает всех людей
     '''
 	i=0
-	for user in inim:
-		print(user,end='-')
+	for user in inim:  #цикл с шагом 1
+		print(user,end='-')  
 		print(mon[i])
 		i+=1
-def poisk(inim:list,mon:list):
+def poisk(inim:list,mon:list): 
     '''Tagastame jarjend või tekst
     :rtype var: tulemus
     '''
-    nimi=(input('Keda otsime?'))
-    for inimene in inim:
-        if inimene==nimi:
-            n=inim.count(nimi)
-            print('Inimene on olemas,selline nimi kohtume',n,'korda')
-            b=0
-            t=[]
-            for i in range(n):
-                k=inim.index(nimi,b)
+    nimi=(input('Keda otsime?'))  #вводим кого найти хотим
+    for inimene in inim:  #цикл с поиском с списках
+        if inimene==nimi:  #проверяем схожи ли имена
+            n=inim.count(nimi)  #Проверяем сколько всего таких людей
+            print('Inimene on olemas,selline nimi kohtume',n,'korda') #показываем пользователю сколько их
+            b=0  
+            t=[]  #создаем пустой списко
+            for i in range(n): 
+                k=inim.index(nimi,b) 
                 palk=mon[k]
                 b+=k+1
                 t.append(nimi+str(palk))   
@@ -40,21 +40,20 @@ def poisk(inim:list,mon:list):
 def udal(inim:list,mon:list):
     '''Удаляет пользователя и его зарплату
     '''
-    a=input('Keda otsime?')
-    if a not in inim:
+    a=input('Keda otsime?')  #спрашиваем человека
+    if a not in inim:    #проверка
         print('See nimi ei ole')
-    else:
-        b=inim.index(a)
-        inim.remove(a)
-        mon.pop(b)
+    else:   
+        b=inim.index(a)     #индекс нашли
+        inim.remove(a)         #Удаляем из списка
+        mon.pop(b)             #Удаляем его зарплату
 def mox(mon:list,inim:list):
     '''Ищет максимальную сумму и кто её получает
     '''
-    mom=max(mon)
-    n=mon.count(mom)
-    for i in mon:
-        if i==mom:
-            b=mon.index(i)
+    mom=max(mon)            #Поиск максимальной зарплаты
+    for i in mon:           #ищём индекс зарплаты
+        if i==mom:        
+            b=mon.index(i)        #запоминаем данный индекс
             break
-    inil=inim[b]
-    print(inil ,'-', mom)
+    inil=inim[b]                #находим человека и запомнаем его в виде переменной
+    print(inil ,'-', mom)         #показываем данного человека и его зарплату
